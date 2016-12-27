717a89fcb71e7db17949bb00f32dd107c5b5dc01
Nick M. Mitchell <nickm@us.ibm.com>
2016-12-27 16:45:03 -0500

    update init.sh to do one-time init, including setup_providers

--- init.sh
+++ init.sh
@@ -2,30 +2,32 @@
 
 . ./conf/config.sh
 
-PROVIDERS=`cat conf/providers.json`
+action=login
 
 npm install >& /dev/null
 
 wsk package create "${PACKAGE}" 2>&1 | grep -v "resource already exists"
+wsk action delete "${PACKAGE}/${action}" 2>&1 | grep -v "resource does not exist"
+wsk action create --kind nodejs:6 "${PACKAGE}/${action}" actions/${action}/${action}.js
+
+# have we already done the initial setup?
+wsk api-experimental list "/${PACKAGE}" | grep "${action}" > /dev/null
+if [ $? == 1 ]; then
+    echo "Performing one-time initialization"
+    ./init-apigw.sh
+    ./bin/setup-providers
+fi
+
+# Update the package with the providers configuration
+PROVIDERS=`cat conf/providers.json`
 wsk package update "${PACKAGE}" -p providers "${PROVIDERS}"
 
-for dir in actions/*/; do
-    action=$(basename "$dir")
 
-    wsk action delete "${PACKAGE}/${action}" 2>&1 | grep -v "resource does not exist"
-    (cd $dir \
-	    && echo "${PACKAGE}/${action}" \
-	    && wsk action create --kind nodejs:6 "${PACKAGE}/${action}" ${action}.js
-    )
 
-    # see if the action needs the full credentials (i.e. including login credentials)
-#    if [ -f "$dir/config.json" ]; then
-#	C=`cat "$dir/config.json" | jq --raw-output .credentials`
-#	if [ "$C" == "full" ]; then
-#	    echo "Using full credentials for ${action}"
-#	    wsk action update "${PACKAGE}/${action}" -p providers "${PROVIDERS}"
-#	fi
-#    fi
-    
-#    rm -f "$dir/node_modules" "$dir/lib"
-done
+
+#for dir in actions/*/; do
+#    action=$(basename "$dir")
+#    wsk action delete "${PACKAGE}/${action}" 2>&1 | grep -v "resource does not exist"
+#    (cd $dir && wsk action create --kind nodejs:6 "${PACKAGE}/${action}" ${action}.js
+#    )
+#done
