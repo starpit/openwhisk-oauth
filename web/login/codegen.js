a92997f368cef8dd514f12925056b0bbdc1aa922
Nick M. Mitchell <nickm@us.ibm.com>
2017-01-05 17:37:11 -0500

    remove unused codegen.js

--- web/login/codegen.js
+++
@@ -1,8 +0,0 @@
-const url = require('url'),
-      path = require('path'),
-      client = require(path.join(__dirname, '..', '..', 'lib', 'client')),
-      providers = require(path.join(__dirname, '..', '..', 'conf', 'providers-client.json'))
-
-for (var providerName in providers) {
-    console.log(`<iframe id="${providerName}" data-provider="${providerName}" data-src="${client.endpoint(providerName).endpoint}"></iframe>`)
-}
