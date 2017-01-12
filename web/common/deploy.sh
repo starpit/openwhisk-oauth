8b2a97825005caae5a0302e4a536444204e72303
Nick M. Mitchell <nickm@us.ibm.com>
2017-01-12 13:31:28 -0500

    add --deploy-only option to deploy.sh

--- web/common/deploy.sh
+++ web/common/deploy.sh
@@ -6,19 +6,33 @@ CONTAINER=public
 
 OS_AUTH=${OS_AUTH-`${OS_HOME}/util/getAuthToken.sh`}
 
-wsk action invoke objectstore/listContainers -b -r -p authToken "${OS_AUTH}" | grep ${CONTAINER} > /dev/null
-if [ $? == 1 ]; then
-    (wsk action invoke objectstore/createContainer -b -r \
-	-p authToken "${OS_AUTH}" \
-	-p container ${CONTAINER}) > /dev/null
+if [ "$2" == "--deploy-only" ]; then
+    DEPLOY_ONLY=1
+fi
 
-    if [ $? == 1 ]; then exit; else echo "Created container ${CONTAINER}"; fi
-fi    
+echo "Deploying `basename $1`"
 
-(wsk action invoke objectstore/setContainerPublic -b -r \
-    -p authToken "${OS_AUTH}" \
-    -p container public) > /dev/null
-if [ $? == 1 ]; then exit; else echo "Made container ${CONTAINER} readable by everyone"; fi
+if [ -z "$DEPLOY_ONLY" ]; then
+    wsk action invoke objectstore/listContainers -b -r -p authToken "${OS_AUTH}" | grep ${CONTAINER} > /dev/null
+    if [ $? == 1 ]; then
+	(wsk action invoke objectstore/createContainer -b -r \
+	     -p authToken "${OS_AUTH}" \
+	     -p container ${CONTAINER}) > /dev/null
+
+	if [ $? == 1 ]; then exit; else echo "Created container ${CONTAINER}"; fi
+    fi
+
+    (wsk action invoke objectstore/setContainerPublic -b -r \
+	 -p authToken "${OS_AUTH}" \
+	 -p container public) > /dev/null
+    if [ $? == 1 ]; then exit; else echo "Made container ${CONTAINER} readable by everyone"; fi
+
+    (wsk action invoke objectstore/setContainerMetadata -b -r \
+	 -p authToken "${OS_AUTH}" \
+	 -p container ${CONTAINER} \
+	 -p metadata  "{\"Access-Control-Allow-Origin\": \"*\", \"Access-Control-Expose-Headers\": \"content-type\"}") > /dev/null
+    if [ $? == 1 ]; then exit; else echo "Added CORS to container ${CONTAINER}"; fi
+fi
 
 REQ=`wsk action invoke objectstore/createObjectAsReq -b -r \
     -p authToken "${OS_AUTH}" \
@@ -27,8 +41,3 @@ REQ=`wsk action invoke objectstore/createObjectAsReq -b -r \
 if [ $? == 1 ]; then exit; else echo "Uploaded content to container ${CONTAINER}"; fi
 node -e "const req = JSON.parse(process.argv[1]); delete req.body; require('fs').createReadStream(\"$1\").pipe(require('request')(req))" "$REQ"
 
-(wsk action invoke objectstore/setContainerMetadata -b -r \
-     -p authToken "${OS_AUTH}" \
-     -p container ${CONTAINER} \
-     -p metadata  "{\"Access-Control-Allow-Origin\": \"*\", \"Access-Control-Expose-Headers\": \"content-type\"}") > /dev/null
-if [ $? == 1 ]; then exit; else echo "Added CORS to container ${CONTAINER}"; fi
