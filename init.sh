151b5c32fbd411077ff2f48b5b1f6c5b98b41ab0
Nick M. Mitchell <nickm@us.ibm.com>
2017-01-19 19:19:48 -0500

    fix for missing CFC_WITH_AUTHZ in init.sh

--- init.sh
+++ init.sh
@@ -22,5 +22,6 @@ fi
 
 ./init-actions.sh
 
+CFC_WITH_AUTHZ="checkForCompletion-with-authz"
 wsk api-experimental delete "/${PACKAGE}" /checkForCompletion 2>&1 | grep -v "does not exist"
 wsk api-experimental create "/${PACKAGE}" /checkForCompletion post "${PACKAGE}/${CFC_WITH_AUTHZ}" 2>&1 | grep -v "already exists"
